32c5555769db353a0e478f6743e032104a95c73b
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-03-07 17:10:16 +0530

    Update and rename cust_filters.py to filters.py

--- DaisyX/modules/filters.py
+++ DaisyX/modules/filters.py
@@ -611,10 +611,11 @@ __help__ = """
 
 *Admin only:*
  ✪ /filter <keyword> <reply message>*:* Add a filter to this chat. The bot will now reply that message whenever 'keyword'\
-is mentioned. If you reply to a sticker with a keyword, the bot will reply with that sticker. NOTE: all filter \
-keywords are in lowercase. If you want your keyword to be a sentence, use quotes. eg: /filter "hey there" How you \
-doin?
- Separate diff replies by `%%%` to get random replies
+   is mentioned. If you reply to a sticker with a keyword, the bot will reply with that sticker. NOTE: all filter \
+   keywords are in lowercase. If you want your keyword to be a sentence, use quotes. eg: `/filter` `"hey there"` `How you \
+   doin?`
+
+Separate diff replies by `%%%` to get random replies
  *Example:* 
  `/filter "filtername"
  Reply 1
@@ -622,6 +623,7 @@ doin?
  Reply 2
  %%%
  Reply 3`
+
  ✪ /stop <filter keyword>*:* Stop that filter.
 
 *Chat creator only:*
