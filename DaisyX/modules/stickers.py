16f928c9ee255f9b1c66e038b060267acfbabd06
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-03-07 19:19:00 +0530

    Update stickers.py

--- DaisyX/modules/stickers.py
+++ DaisyX/modules/stickers.py
@@ -452,10 +452,10 @@ def makepack_internal(
 
 
 __help__ = """
-• `/stickerid`*:* reply to a sticker to me to tell you its file ID.
-• `/getsticker`*:* reply to a sticker to me to upload its raw PNG file.
-• `/kang`*:* reply to a sticker to add it to your pack.
-• `/stickers`*:* Find stickers for given term on combot sticker catalogue
+• /stickerid*:* reply to a sticker to me to tell you its file ID.
+• /getsticker*:* reply to a sticker to me to upload its raw PNG file.
+• /kang*:* reply to a sticker to add it to your pack.
+• /stickers*:* Find stickers for given term on combot sticker catalogue
 """
 
 __mod_name__ = "Stickers 🎉"
