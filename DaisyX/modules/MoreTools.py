a74eee129acdb4df0a93932f610397e3ec281458
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-03-07 19:05:36 +0530

    Update MoreTools.py

--- DaisyX/modules/MoreTools.py
+++ DaisyX/modules/MoreTools.py
@@ -1,21 +1,21 @@
 __help__ = """
 ** GPS **
- - /gps <Place> : Show Location on a map
+ - /gps `<place>` : Show Location on a map
  
  ** Blue Cleaner **
- - /cleanblue on : Turn bluetext cleaner on
- - /cleanblue off : Turn bluetext cleaner off
+ - /cleanblue `on` : Turn bluetext cleaner on
+ - /cleanblue `off` : Turn bluetext cleaner off
  
  ** Send **
- - /snd <Text> : Message like the bot
+ - /snd `<Text>` : Message like the bot
  
 ** Grammer **
- - /t <reply> : Show grammer corrected text
+ - /t `<reply>` : Show grammer corrected text
  
 ** Image Tools**
- - /img <text>: perform a image search
- - /getqr <reply to image>: Read QR code
- - /makeqr <reply to text>: Make QR code
+ - /img `<text>`: perform a image search
+ - /getqr `<reply to image>`: Read QR code
+ - /makeqr `<reply to text>`: Make QR code
  
 ** Style Text **
  - /weebify : Weebify Text
@@ -23,6 +23,6 @@ __help__ = """
  - /blue : Blues text
  
 ** More **
- - /phone <tp no> : Track Phone no
+ - /phone `<phone number>` : Track Phone no
 """
 __mod_name__ = "More Tools 🧩"
