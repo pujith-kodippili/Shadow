b4d7811f6e46117e4a667a105e826a660eaf9a58
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-03-07 14:08:56 +0530

    Update anime.py

--- DaisyX/modules/anime.py
+++ DaisyX/modules/anime.py
@@ -569,18 +569,18 @@ def kayo(update: Update, context: CallbackContext):
     site_search(update, context, "kayo")
 
 
-__help__ = """
+__help__ = ""
 Get information about anime, manga or characters from [AniList](anilist.co).
 *Available commands:*
- • `/anime <anime>`*:* returns information about the anime.
- • `/character <character>`*:* returns information about the character.
- • `/animequote` *:* Get random Anime qoute.
- • `/manga <manga>`*:* returns information about the manga.
- • `/user <user>`*:* returns information about a MyAnimeList user.
- • `/upcoming`*:* returns a list of new anime in the upcoming seasons.
- • `/kaizoku <anime>`*:* search an anime on animekaizoku.com
- • `/kayo <anime>`*:* search an anime on animekayo.com
- • `/airing <anime>`*:* returns anime airing info.
+ • /anime `<anime>`*:* returns information about the anime.
+ • /character `<character>`*:* returns information about the character.
+ • /animequote *:* Get random Anime qoute.
+ • /manga `<manga>`*:* returns information about the manga.
+ • /user `<user>`*:* returns information about a MyAnimeList user.
+ • /upcoming*:* returns a list of new anime in the upcoming seasons.
+ • /kaizoku `<anime>`*:* search an anime on animekaizoku.com
+ • /kayo `<anime>`*:* search an anime on animekayo.com
+ • /airing `<anime>`*:* returns anime airing info.
  """
 
 ANIME_HANDLER = DisableAbleCommandHandler("anime", anime)
