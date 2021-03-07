8142896572ce1b10744a9ad480c31406983af0e1
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-03-07 17:17:46 +0530

    Update welcome.py

--- DaisyX/modules/welcome.py
+++ DaisyX/modules/welcome.py
@@ -1013,9 +1013,9 @@ WELC_HELP_TXT = (
 WELC_MUTE_HELP_TXT = (
     "You can get the bot to mute new people who join your group and hence prevent spambots from flooding your group. "
     "The following options are possible:\n"
-    "• `/welcomemute soft`*:* restricts new members from sending media for 24 hours.\n"
-    "• `/welcomemute strong`*:* mutes new members till they tap on a button thereby verifying they're human.\n"
-    "• `/welcomemute off`*:* turns off welcomemute.\n"
+    "• /welcomemute soft*:* restricts new members from sending media for 24 hours.\n"
+    "• /welcomemute strong*:* mutes new members till they tap on a button thereby verifying they're human.\n"
+    "• /welcomemute off*:* turns off welcomemute.\n"
     "*Note:* Strong mode kicks a user from the chat if they dont verify in 120seconds. They can always rejoin though"
 )
 
@@ -1061,19 +1061,20 @@ def __chat_settings__(chat_id, user_id):
 
 __help__ = """
 *Admins only:*
- ✪ /welcome <on/off>*:* enable/disable welcome messages.
+ ✪ /welcome `<on/off>`*:* enable/disable welcome messages.
  ✪ /welcome*:* shows current welcome settings.
  ✪ /welcome noformat*:* shows current welcome settings, without the formatting - useful to recycle your welcome messages!
  ✪ /goodbye*:* same usage and args as `/welcome`.
- ✪ /setwelcome <sometext>*:* set a custom welcome message. If used replying to media, uses that media.
- ✪ /setgoodbye <sometext>*:* set a custom goodbye message. If used replying to media, uses that media.
+ ✪ /setwelcome `<sometext>`*:* set a custom welcome message. If used replying to media, uses that media.
+ ✪ /setgoodbye `<sometext>`*:* set a custom goodbye message. If used replying to media, uses that media.
  ✪ /resetwelcome*:* reset to the default welcome message.
  ✪ /resetgoodbye*:* reset to the default goodbye message.
- ✪ /cleanwelcome <on/off>*:* On new member, try to delete the previous welcome message to avoid spamming the chat.
+ ✪ /cleanwelcome `<on/off>`*:* On new member, try to delete the previous welcome message to avoid spamming the chat.
  ✪ /welcomemutehelp*:* gives information about welcome mutes.
- ✪ /cleanservice <on/off*:* deletes telegrams welcome/left service messages. 
+ ✪ /cleanservice `<on/off>`*:* deletes telegrams welcome/left service messages. 
+ 
  *Example:*
-user joined chat, user left chat.
+  user joined chat, user left chat.
 
 *Welcome markdown:* 
  ✪ `/welcomehelp`*:* view more formatting information for custom welcome/goodbye messages.
