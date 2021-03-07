c3c17b2a05039819ebc52a8ce384800e2b171cb4
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-03-07 19:13:23 +0530

    Update rules.py

--- DaisyX/modules/rules.py
+++ DaisyX/modules/rules.py
@@ -122,7 +122,7 @@ __help__ = """
  ✪ /rules*:* get the rules for this chat.
 
 *Admins only:*
- ✪ /setrules <your rules here>*:* set the rules for this chat.
+ ✪ /setrules `<your rules here>`*:* set the rules for this chat.
  ✪ /clearrules*:* clear the rules for this chat.
 """
 
